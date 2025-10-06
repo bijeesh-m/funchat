81610105e5d47fff08caccaa063c9e85fc506227
Bijeesh <bijeeshh.m@gmail.com>
2025-10-06 23:52:03 +0530

    fix:migrated to vite

--- server/server.js
+++ server/server.js
@@ -25,10 +25,10 @@ const io = new Server(server, {
 
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 let users = [];
